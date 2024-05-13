e3d953763c18c6fc52d9abe2e7eeefd707ddfe9c
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-12 19:40:42 -0700

    Change index file to include new sections for ui
    Change JavaScript to incorporate functions for
    modifying DOM elements
    
    Add CSS file to create aesthetically pleasing ui

--- source.js
+++ source.js
@@ -2,86 +2,105 @@
 let userInput;
 //create variable for computer choice
 let computerChoice
-//create array of choices
-let stringArray = ["Rock", "Paper", "Scissors"]
 let humanScore = 0;
 let computerScore = 0;
 let errorThrown = false;
+let stringArray = ["Rock","Paper","Scissors"]
+
+//Initialize buttons for each selection
+const rockBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+
+paperBtn.textContent = "Paper";
+scissorsBtn.textContent = "Scissors";
+rockBtn.textContent = "Rock";
+
+const body = document.querySelector(".button-container");
+
+const divElem = document.querySelector(".results");
+const divElem2 = document.querySelector(".computer-results")
+const human = document.querySelector("#human-score");
+const computer = document.querySelector("#computer-score")
+
+body.appendChild(rockBtn);
+body.appendChild(paperBtn);
+body.appendChild(scissorsBtn);
+
+rockBtn.addEventListener("click", () => {
+    userInput = "rock";
+    playRound(userInput,getComputerChoice())
+}); 
+paperBtn.addEventListener("click", () => {
+    userInput = "paper";
+    playRound(userInput,getComputerChoice())
+});
+scissorsBtn.addEventListener("click", () => {
+    userInput = "scissors";
+    playRound(userInput,getComputerChoice())
+})
+
 //create a random computer choice
 function getComputerChoice() {
     minNum = 0;
     maxNum = 2;
     computerChoice = stringArray[Math.floor(Math.random() * (maxNum + 1))].toLowerCase();
-    console.log(`Computer choice is: ${computerChoice}`)
+    divElem2.textContent = `Computer choice is: ${computerChoice}`;
     return computerChoice;
 }
-//get user input
-function getHumanChoice() {
-    userInput = prompt("What would you like to play?").toLowerCase();
-    return userInput; 
-}
+
 
 //Play a round
 function playRound(humanChoice, computerChoice) {
     if(humanChoice == computerChoice)
     {
-        console.log('It\'s a tie!');
+        divElem.textContent = 'It\'s a tie!';
     }
     else if (humanChoice == 'rock' && computerChoice == 'paper')
     {
-        console.log(`You lose! Paper beats Rock!`)
+        divElem.textContent = `You lose! Paper beats Rock!`
         ++computerScore;
     }
     else if(humanChoice == 'scissors' && computerChoice == 'rock')
     {
-        console.log(`You lose! Rock beats Scissors!`)
+        divElem.textContent = `You lose! Rock beats Scissors!`;
         ++computerScore;
     }
     else if(humanChoice == 'paper' && computerChoice == 'scissors')
     {
-        console.log(`You lose! Scissors beat Paper!`)
+        divElem.textContent =`You lose! Scissors beat Paper!`;
         ++computerScore;
     }
     else if (humanChoice == 'paper' && computerChoice == 'rock')
     {
-        console.log(`You win! Paper beats Rock!`)
+        divElem.textContent = `You win! Paper beats Rock!`;
         ++humanScore;
     }
     else if(humanChoice == 'rock' && computerChoice == 'scissors')
     {
-        console.log(`You win! Rock beats Scissors!`)
+        divElem.textContent = `You win! Rock beats Scissors!`;
         ++humanScore;
     }
     else if(humanChoice == 'scissors' && computerChoice == 'paper')
     {
-        console.log(`You win! Scissors beat Paper!`)
+        divElem.textContent = `You win! Scissors beat Paper!`;
         ++humanScore;
     }
-    else if(humanChoice != stringArray.indexOf(humanChoice))
-    {
-        console.log('Oops, you didn\'t type a correct response.');
-    }
-
+    human.textContent = "Your score is : " + humanScore;
+    computer.textContent = "The computer's score is: " + computerScore;
+    if(computerScore === 5)
+        {
+            humanScore = 0;
+            computerScore = 0;
+            divElem2.textContent = ''
+            divElem.textContent = "Computer Wins!"
+        }
+        else if (humanScore === 5)
+        {
+            humanScore = 0;
+            computerScore = 0;
+            divElem2.textContent = ''
+            divElem.textContent = "You Win!"
+        }
 }
 
-
-
-//Play a game
-function playGame(){
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-
-    if(computerScore > humanScore)
-    {
-        console.log('Computer Wins! Try again!')
-    }
-    else if (computerScore < humanScore)
-    {
-        console.log('You win!!')
-    }
-
-}
-playGame();
